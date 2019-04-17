8af3025b3ca1f3e1cb54417056eb426254b36970
xinyuezhang93 <zhang.xinyue@husky.neu.edu>
2019-04-17 11:53:44 -0700

    amd

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -8,7 +8,8 @@ const NotFound = () => (
     <h1>Wrong way! </h1>
     <h2>Please Drive Back to <Link to={`/`} className = {styles.link}>HomePage</Link>
     <br></br>
-    Or have a rest here.</h2></div>
+    Or have a rest here.</h2>
+    <h2>Visit <Link to={`/about`} className={styles.contact_about}>About Us</Link> or <Link to={`/contact`} className={styles.contact_about}>Contact Us</Link> </h2></div>
     <div className = {styles.background_image}></div>
   </div>
 );
